4161fbd8302caba75188b811c22b7e2a863f3fdd
Abran Rodriguez Jr <abranrodriguezjr@gmail.com>
2022-08-22 09:01:49 -0500

    practice

--- js/practice_js.js
+++ js/practice_js.js
@@ -1,17 +1,55 @@
 //concat
-let firstName = "Abran";
-let lastName = "Rodriguez";
-let car1 = "Dodge Caravan GT";
-let car2 = "Chevy Avalanche"
-let car3 = "Ford Edge SEL";
-let car4 = "Hyundai Sonata"
-let streetAddress = "509 Denise Dr";
-let city = "New Braunfels";
-let state = "Texas";
-let school = "Codeup";
+// let firstName = "Abran";
+// let lastName = "Rodriguez";
+// let car1 = "Dodge Caravan GT";
+// let car2 = "Chevy Avalanche"
+// let car3 = "Ford Edge SEL";
+// let car4 = "Hyundai Sonata"
+// let streetAddress = "509 Denise Dr";
+// let city = "New Braunfels";
+// let state = "Texas";
+// let school = "Codeup";
+//
+// function intro(){
+//     return `Hi my name is ${firstName} ${lastName}. I live at \n ${streetAddress} ${city}, ${state}. I just started going to ${school} for web development. I had two vehicles but on the second day I got into an accident. I was driving my ${car2} on the way home and someone lane changed to avoid an accident and collided into me. My ${car2} was totaled. I was fine after a day or two and now driving a rental, ${car4}. I don't like it. I'm hoping to buy a ${car3}.`
+// }
+//
+// console.log(intro());
+//
+// const person = {
+//     name: "John", age: 50
+// }
+// alert(person.name + " is " + person.age);
+//
+// let txt = "Hello World!";
+// let x = txt.length;
+// alert(x);
+//
+// let text = "We are Dem' \"Boys!\"";
+//     alert(text);
+// let emphasize = text.slice(7, 18).toUpperCase();
+// alert(`I SAID WE ${emphasize}`);
+//
+// const fruits = ["Banana", "Orange", "Apple"];
+//
+// const d = new Date();
+// alert(d);
+// year = d.getFullYear();
+// month = d.getMonth();
+//
+// d.setFullYear(2020);
+//Math.random();
+//Math.max(); Picks largest integer
+//Math.round(); Rounds to nearest integer
+//Math.sqrt(); Gets square root of integer
 
-function intro(){
-    return `Hi my name is ${firstName} ${lastName}. I live at \n ${streetAddress} ${city}, ${state}. I just started going to ${school} for web development. I had two vehicles but on the second day I got into an accident. I was driving my ${car2} on the way home and someone lane changed to avoid an accident and collided into me. My ${car2} was totaled. I was fine after a day or two and now driving a rental, ${car4}. I don't like it. I'm hoping to buy a ${car3}.`
-}
+// let age = n;
+// let voteable = (age < 18) ? "Too young" : "Old enough"; alert(votealbe); - Ternary
 
-console.log(intro());
+// function ifGreater(x, y){
+//     if ( Math.round(x) > Math.round(y)) {
+//         alert(`${x} is greater then ${y}.`);
+//     } else {
+//         alert(`${y} is greater than ${x}.`);
+//     }
+// }
