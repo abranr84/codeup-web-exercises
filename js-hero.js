7b3957402b316528712a4fa4d160559763913afb
Abran Rodriguez Jr <abranrodriguezjr@gmail.com>
2022-09-01 13:33:49 -0500

    js hero

---
+++ js-hero.js
@@ -0,0 +1,27 @@
+let firstname = "Lata"
+
+//Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'.
+
+let flower = "rose";
+let tree = "maple";
+
+//Which value does x have after execution of the following code?
+// let x = 'Tic';
+// x = 'Tac';
+// x = 'Toe';
+
+//'Toe'
+
+//Which value does x have after execution of the following code?
+// let x = 'Laurel';
+// let y = 'Hardy';
+// let z = y;
+// y = x;
+// x = z;
+
+//'Hardy'
+
+//Define a function hello that returns 'Hello world!'.
+function hello(){
+    return 'Hello world!'
+}
